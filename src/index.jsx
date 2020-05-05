a3987cc0413b6fcf99f4d0de7c390ee3652bd67a
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-05 20:38:23 +0300

    feat: [3-26] learn jsx features

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => (
-  <ul>
-    <li>Learn React</li>
-    <li>Create ToDo App</li>
-  </ul>
-);
+const TodoList = () => {
+  const items = ['Learn React', 'Create ToDo App'];
+  return (
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+};
 
 const AppHeader = () => (
   <h1>My Todo List</h1>
 );
 
-const SearchPanel = () => (
-  <input placeholder="search what to do" />
-);
+const SearchPanel = () => {
+  const placeholder = 'Search what to do';
+  const style = {
+    fontSize: '20px',
+  };
+
+  return (
+    <input
+      style={style}
+      placeholder={placeholder}
+    />
+  );
+};
 
 const App = () => (
   <div>
