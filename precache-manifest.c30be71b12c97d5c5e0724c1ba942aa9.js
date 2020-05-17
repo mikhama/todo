dc378edc1882ff3a0f75fcf3c9679908ef7f8e00
Dzmitry Tsebruk <mikhama@tut.by>
2020-05-17 22:31:05 +0300

    Updates

---
+++ precache-manifest.c30be71b12c97d5c5e0724c1ba942aa9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d315e1aabf873220f3b99ff3fb7bb503",
+    "url": "./todo/index.html"
+  },
+  {
+    "revision": "ca2f7825dac699f704cc",
+    "url": "./todo/static/css/2.6642d070.chunk.css"
+  },
+  {
+    "revision": "94dc648d3fb68a8f8851",
+    "url": "./todo/static/css/main.5301b609.chunk.css"
+  },
+  {
+    "revision": "ca2f7825dac699f704cc",
+    "url": "./todo/static/js/2.ae2acc24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./todo/static/js/2.ae2acc24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dc648d3fb68a8f8851",
+    "url": "./todo/static/js/main.2ac2651d.chunk.js"
+  },
+  {
+    "revision": "f3fec178c66db8aa4841",
+    "url": "./todo/static/js/runtime-main.a1ceb84f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./todo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./todo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./todo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./todo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./todo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo/precache-manifest.efd45215007347e0d0154d1bea700ed0.js"
+  "./todo/precache-manifest.c30be71b12c97d5c5e0724c1ba942aa9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
